4b8b7b4643689436cf8439beb3beeb150c0610b3
josetitoarchila <titoarchila@gmail.com>
2022-12-01 20:00:40 -0500

    JavaScript

---
+++ js/libros.js
@@ -0,0 +1,13 @@
+/*let a=prompt("ingresar numero 1");
+let b=prompt("ingresar numero 2");
+
+let sumatoria= parseInt(a)+parseInt(b);
+alert("el valor de a es "+sumatoria);
+
+let c= prompt("ingresar numero 3 float");
+let d= prompt("ingresar numero 4 float");
+let sumatoria3 = parseFloat(c)+parseFloat(d);
+alert("el valor de sumatoria2 es "+sumatoria3);*/
+document.write("<h5 style='text-align:center; color:orange'>titulo h5 cambios</h5>");
+
+
